1a4f602778e90d33b8356560b8629ef3e94d6d73
Josiah Clark <josiah1121@gmail.com>
2025-12-24 01:01:27 -0500

    Updating linter config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,14 @@
 // eslint.config.js
+import globals from "globals";
+
 export default [
     {
+        languageOptions: {
+            // This adds 'window', 'document', and other browser globals
+            globals: {
+                ...globals.browser
+            }
+        },
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "error",
